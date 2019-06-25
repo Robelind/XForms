71ee7be56a4abe56b4675cf99cbff4f05e2eac31
Robert Lindblom <robert@rlsolutions.se>
2019-06-25 10:16:40 +0200

    -

---
+++ demo/XFormsDemo/XFormsDemo/Attributes/RequiredIfFalseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using XForms.Attributes;
+
+namespace XFormsDemo.Attributes
+{
+    class RequiredIfFalseViewModel
+    {
+        public RequiredIfFalseViewModel()
+        {
+            CommitCmd = new Command(() => Application.Current.MainPage.DisplayAlert("XForms", "Form was committed!", "Ok"));
+        }
+
+        public ICommand CommitCmd { get; set; }
+
+        public bool Flag { get; set; }
+        [RequiredIfFalse("Flag")]
+        public string Value { get; set; }
+    }
+}
--- demo/XFormsDemo/XFormsDemo/Attributes/RequiredTrueViewModel.cs
+++ demo/XFormsDemo/XFormsDemo/Attributes/RequiredTrueViewModel.cs
@@ -4,9 +4,9 @@ using XForms.Attributes;
 
 namespace XFormsDemo.Attributes
 {
-    class AttributesViewModel
+    class RequiredTrueViewModel
     {
-        public AttributesViewModel()
+        public RequiredTrueViewModel()
         {
             CommitCmd = new Command(() => Application.Current.MainPage.DisplayAlert("XForms", "Form was committed!", "Ok"));
         }
@@ -15,11 +15,5 @@ namespace XFormsDemo.Attributes
 
         [RequiredTrue]
         public bool MustCheck { get; set; }
-        public bool Value1Flag { get; set; }
-        [RequiredIfTrue("Value1Flag")]
-        public string Value1 { get; set; }
-        public bool Value2Flag { get; set; }
-        [RequiredIfFalse("Value2Flag")]
-        public string Value2 { get; set; }
     }
 }
